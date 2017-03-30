8db616099e8424f39374332db28822fcb0f60c74
ellasw <ellawilson13@gmail.com>
2017-03-30 12:33:55 -0700

    Update emp-account-update.php

--- emp-account-update.php
+++ emp-account-update.php
@@ -1,3 +1,110 @@
+<?php 
+$email = $_GET['emp_email'];
+$success = True; //keep track of errors so it redirects the page only if there are no errors
+$db_conn = OCILogon("ora_a2v9a", "a17792145", "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = dbhost.ugrad.cs.ubc.ca)(PORT = 1522)))(CONNECT_DATA=(SID=ug)))");
+function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
+	//echo "<br>running ".$cmdstr."<br>";
+	global $db_conn, $success;
+	$statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+	if (!$statement) {
+		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn); // For OCIParse errors pass the       
+		// connection handle
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+	$r = OCIExecute($statement, OCI_DEFAULT);
+	if (!$r) {
+		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+		echo htmlentities($e['message']);
+		$success = False;
+	} else {
+	}
+	return $statement;
+}
+function executeBoundSQL($cmdstr, $list) {
+	/* Sometimes a same statement will be excuted for severl times, only
+	 the value of variables need to be changed.
+	 In this case you don't need to create the statement several times; 
+	 using bind variables can make the statement be shared and just 
+	 parsed once. This is also very useful in protecting against SQL injection. See example code below for how this functions is used */
+	global $db_conn, $success;
+	$statement = OCIParse($db_conn, $cmdstr);
+	if (!$statement) {
+		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn);
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+	foreach ($list as $tuple) {
+		foreach ($tuple as $bind => $val) {
+			//echo $val;
+			//echo "<br>".$bind."<br>";
+			OCIBindByName($statement, $bind, $val);
+			unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+		}
+		$r = OCIExecute($statement, OCI_DEFAULT);
+		if (!$r) {
+			echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+			$e = OCI_Error($statement); // For OCIExecute errors pass the statementhandle
+			echo htmlentities($e['message']);
+			echo "<br>";
+			$success = False;
+		}
+	}
+}
+function printResult($result) { //prints results from a select statement
+	//echo "<br>Resulting Data:<br>";
+	echo "<table>";
+	echo "<tr><th>ID</th><th>Name</th></tr>";
+	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+		echo "<tr><td>" . $row["NID"] . "</td><td>" . $row["NAME"] . "</td></tr>"; //or just use "echo $row[0]" 
+	}
+	echo "</table>";
+}
+function printEmployee($result) {
+	echo "<br> All Employees: </br>";
+	echo "<table>";
+	echo "<tr>
+            <th>Name:</th>
+            <th>Email:</th>
+            <th>Branch #:</th>
+        </tr>";
+        
+    while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+     echo "<tr><td>" . $row["NAME"] . "</td><td>" . $row["EMP_EMAIL"] . "</td><td>" . $row["BRANCH_NO"] . "</td></tr>";
+    }
+    echo "</table>";
+}
+function printCustomer($result) { //prints results from a select statement
+    echo "<br>Got data from table customer:<br>";
+    echo "<table>";
+    echo "<tr><th>Email</th><th>Name</th></tr>";
+    while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+        echo "<tr><td>" . $row["CUST_EMAIL"] . "</td><td>" . $row["CUST_NAME"] . "</td></tr>"; //or just use "echo $row[0]"
+    }
+    echo "</table>";
+}
+function printBranch($result) {
+	echo "<p> Existing Branches:</p>";
+	echo "<table>";
+	echo "<tr><th>Branch #:</th><th>Address:</th><th>City:</th><th>Province:</th><th>Postal Code:</th></tr>";
+	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+        echo "<tr><td>" . $row["BRANCH_NO"] . "</td><td>" . $row["STREET_NO"] . "</td><td>" . $row["CITY"] . "</td><td>" . $row["PROVINCE"] . "</td><td>" . $row["POSTAL_CODE"] . "</td></tr>"; //or just use "echo $row[0]"
+    }
+    echo "</table>";
+}
+
+?>
+
+
+<form method="POST" action="emp-account-update.php?emp_email=<?php echo $email;?>">
+    <div>
+		<input type="submit" value="Back To Browse" name="back_to_browse"/>
+    </div>
+
+
 <p> <font size = "5"> Employee Update Page</font> </p>
 
 <p> <font size = "3" color = "medblue">Edit Existing Employee Account: </font> </p>
@@ -82,133 +189,14 @@
 <input type="submit" value="show all branches" name="ShowBranch">
 </form>
 <?php
-
 //this tells the system that it's no longer just parsing 
 //html; it's now parsing PHP
-
-$success = True; //keep track of errors so it redirects the page only if there are no errors
-$db_conn = OCILogon("ora_a2v9a", "a17792145", "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = dbhost.ugrad.cs.ubc.ca)(PORT = 1522)))(CONNECT_DATA=(SID=ug)))");
-
-function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
-	//echo "<br>running ".$cmdstr."<br>";
-	global $db_conn, $success;
-	$statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
-
-	if (!$statement) {
-		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-		$e = OCI_Error($db_conn); // For OCIParse errors pass the       
-		// connection handle
-		echo htmlentities($e['message']);
-		$success = False;
-	}
-
-	$r = OCIExecute($statement, OCI_DEFAULT);
-	if (!$r) {
-		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
-		echo htmlentities($e['message']);
-		$success = False;
-	} else {
-
-	}
-	return $statement;
-
-}
-
-function executeBoundSQL($cmdstr, $list) {
-	/* Sometimes a same statement will be excuted for severl times, only
-	 the value of variables need to be changed.
-	 In this case you don't need to create the statement several times; 
-	 using bind variables can make the statement be shared and just 
-	 parsed once. This is also very useful in protecting against SQL injection. See example code below for how this functions is used */
-
-	global $db_conn, $success;
-	$statement = OCIParse($db_conn, $cmdstr);
-
-	if (!$statement) {
-		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-		$e = OCI_Error($db_conn);
-		echo htmlentities($e['message']);
-		$success = False;
-	}
-
-	foreach ($list as $tuple) {
-		foreach ($tuple as $bind => $val) {
-			//echo $val;
-			//echo "<br>".$bind."<br>";
-			OCIBindByName($statement, $bind, $val);
-			unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
-
-		}
-		$r = OCIExecute($statement, OCI_DEFAULT);
-		if (!$r) {
-			echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-			$e = OCI_Error($statement); // For OCIExecute errors pass the statementhandle
-			echo htmlentities($e['message']);
-			echo "<br>";
-			$success = False;
-		}
-	}
-
-}
-
-function printResult($result) { //prints results from a select statement
-	//echo "<br>Resulting Data:<br>";
-	echo "<table>";
-	echo "<tr><th>ID</th><th>Name</th></tr>";
-
-	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-		echo "<tr><td>" . $row["NID"] . "</td><td>" . $row["NAME"] . "</td></tr>"; //or just use "echo $row[0]" 
-	}
-	echo "</table>";
-
-}
-
-
-function printEmployee($result) {
-	echo "<br> All Employees: </br>";
-	echo "<table>";
-	echo "<tr>
-            <th>Name:</th>
-            <th>Email:</th>
-            <th>Branch #:</th>
-        </tr>";
-        
-    while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-     echo "<tr><td>" . $row["NAME"] . "</td><td>" . $row["EMP_EMAIL"] . "</td><td>" . $row["BRANCH_NO"] . "</td></tr>";
-    }
-    echo "</table>";
-
-}
-
-function printCustomer($result) { //prints results from a select statement
-    echo "<br>Got data from table customer:<br>";
-    echo "<table>";
-    echo "<tr><th>Email</th><th>Name</th></tr>";
-    while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-        echo "<tr><td>" . $row["CUST_EMAIL"] . "</td><td>" . $row["CUST_NAME"] . "</td></tr>"; //or just use "echo $row[0]"
-    }
-    echo "</table>";
-}
-
-function printBranch($result) {
-	echo "<p> Existing Branches:</p>";
-	echo "<table>";
-	echo "<tr><th>Branch #:</th><th>Address:</th><th>City:</th><th>Province:</th><th>Postal Code:</th></tr>";
-	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-        echo "<tr><td>" . $row["BRANCH_NO"] . "</td><td>" . $row["STREET_NO"] . "</td><td>" . $row["CITY"] . "</td><td>" . $row["PROVINCE"] . "</td><td>" . $row["POSTAL_CODE"] . "</td></tr>"; //or just use "echo $row[0]"
-    }
-    echo "</table>";
-}
-
 // Connect Oracle...
 if ($db_conn) {
 // 				$result = executePlainSQL("select * from customer");
 // 				printCustomer($result);
 				// 				$result = executePlainSQL("select * from branch");
 				// printBranch($result);
-
-
 	// $result = executePlainSQL("select * from branch_employee");
 	//  	printEmployee($result);
 	
@@ -249,7 +237,6 @@ if ($db_conn) {
 			$result = executePlainSQL("select * from branch_employee");
 			printEmployee($result);
 		} 
-
 		else if (array_key_exists('UpdateCustAccount', $_POST)) {
 					$CustEmailUp   = $_POST['CustEmailUp'];
 					$NewCustName = $_POST['NewCustName'];
@@ -274,7 +261,6 @@ if ($db_conn) {
 				}
 				$result = executePlainSQL("select * from customer");
 				printCustomer($result);
-
 				}
 				
 		else if (array_key_exists('DeleteEmpAccount', $_POST)) {
@@ -305,7 +291,6 @@ if ($db_conn) {
 				OCICommit($db_conn);
 				$result = executePlainSQL("select * from branch");
 				printBranch($result);
-
 					}
 		}
 		
@@ -331,8 +316,11 @@ if ($db_conn) {
 		} else if (array_key_exists('ShowBranch', $_POST)) {
 			$result = executePlainSQL("select * from branch");
 			printBranch($result);
-
 		}
+		elseif(array_key_exists('back_to_browse', $_POST)){
+			header("location: emp_browse.php?emp_email=" . $email);
+		}
+		
 		
 		 
 // 		if($_POST && success){
@@ -348,6 +336,4 @@ if ($db_conn) {
 	$e = OCI_Error(); // For OCILogon errors pass no handle
 	echo htmlentities($e['message']);
 }
-
 ?>
-
